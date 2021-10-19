7910c5ecac651f002c6ef4b6db1dadbf40801332
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-10-19 23:05:35 +0300

    Read data from firebase and show it in a label

--- FirestoreExample/ViewController.swift
+++ FirestoreExample/ViewController.swift
@@ -15,20 +15,44 @@
 import FirebaseFirestore
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //create a database
     let database = Firestore.firestore()
     
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let field: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Enter text..."
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.black.cgColor
+        return field
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        writeData(text: "othman")
+        view.addSubview(label)
+        view.addSubview(field)
+        //We need to get the event when the user hit return button
+        field.delegate = self
+       
         //read data
         let docRef = database.document("diajo/example")
         docRef.getDocument() { (snapshot,error) in
             
             guard let data = snapshot?.data(), error == nil else { return }
-            print(data)
+            guard let text = data["text"] as? String else { return }
             
+            //update ui with our data saved in firebase
+            DispatchQueue.main.async { [weak self] in
+                self?.label.text = text
+            }
         }
         
     }
@@ -40,6 +64,28 @@ class ViewController: UIViewController {
         let docRef = database.document("diajo/example")
         //set data as dicitionary
         docRef.setData(["text": text])
+    }
+    
+    //assign some frames to label and field
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        field.frame = CGRect(x: 10, y: view.safeAreaInsets.top+10
+                             , width: view.frame.size.width-20,
+                             height: 50)
+        
+        label.frame = CGRect(x: 10, y: view.safeAreaInsets.top+10+60
+                             , width: view.frame.size.width-20,
+                             height: 50)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //make sure text isnt empty then save data
+        if let text = textField.text, !text.isEmpty {
+            writeData(text: text)
+            
+        }
+        return true
     }
 
     
