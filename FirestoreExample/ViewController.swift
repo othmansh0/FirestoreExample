729d1173ecf1cebf0ea26b957741e4f351ce43b2
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-10-19 18:58:40 +0300

    Setup connection with firebase and implement writeData method

--- FirestoreExample/ViewController.swift
+++ FirestoreExample/ViewController.swift
@@ -5,15 +5,32 @@
 //  Created by othman shahrouri on 10/19/21.
 //
 
+//each document have a dictionary in it which will hold the values
+import FirebaseFirestore
 import UIKit
 
 class ViewController: UIViewController {
-
+    //create a database
+    let database = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//to read and write anything to the database we need to have a document reference
+        
+    }
+    
+  
+    func writeData(text: String){
+        //gets a document reference at specified path in database
+        //"collection/document"
+        let docRef = database.document("diajo/example")
+        //set data as dicitionary
+        docRef.setData(["text": text])
     }
 
+    
+    
 
 }
 
+ 
